c2c2ab311cfbc6664aeb922775974ed5a9b034ca
gobiengineer <37341503+gobiengineer@users.noreply.github.com>
2024-02-16 20:35:50 +0530

    Add files via upload

---
+++ Week1/Day1/Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion = 100.2F;
+String browserName = "Firefox";
+boolean pageLoaded = true;
+int noOfBrowser = 1;
+char browserLogo = 'F';
+System.out.println("Browser Version: "+browserVersion+ "\n" + "Browser Name: " +browserName+ "\n"+ "Page Loaded: "+pageLoaded+ "\n"+ "Number of Browser: "+noOfBrowser+ "\n" + "browserLogo: "+browserLogo);
+
+
+	}
+
+}
